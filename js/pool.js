bc0a83610e0da9b5cbe26e3c1b0a1fa277c76ea9
apistingweb <143703659+apistingweb@users.noreply.github.com>
2025-09-17 11:37:43 +0300

    Create pool.js

---
+++ js/pool.js
@@ -0,0 +1,7 @@
+console.group("%cSTING WEB - Yalla Shoot Pro Template", "font-weight:500;color:#f50;font-size:20px;");
+console.log("=> Designed by      : STING WEB");
+console.log("=> FB Page URL      : https://fb.com/stingweb.eg");
+console.log("=> Version          : 2024 / 2.0 - STABLE");
+console.log("=> Desgin URL       : https://www.sting-web.com");
+console.log("=> %cاطلب نسخة لموقعك الان  - من خلال ستينج ويب", "font-weight:500;color:#f50;font-size:15px;");
+console.groupEnd();
